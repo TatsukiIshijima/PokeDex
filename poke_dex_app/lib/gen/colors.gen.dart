4383783d3ebe1edd2fc57603a4a6d7b35800c188
ishijimaitsuki <tatsuki445@gmail.com>
2021-07-26 17:33:00 +0900

    generate define color file by flutter_gen(fvm flutter packages pub run build_runner build)

---
+++ poke_dex_app/lib/gen/colors.gen.dart
@@ -0,0 +1,55 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  static const Color background = Color(0xFF2B292B);
+  static const Color background800 = Color(0xFF424242);
+  static const Color background900 = Color(0xFF212121);
+  static const Color black = Color(0xFF000000);
+  static const Color black70 = Color(0xB3000000);
+  static const Color bug = Color(0xFF179A55);
+  static const Color colorAccent = Color(0xFFD53A47);
+  static const Color colorPrimary = Color(0xFFD53A47);
+  static const Color colorPrimaryDark = Color(0xFFD53A47);
+  static const Color dark = Color(0xFF040706);
+  static const Color dragon = Color(0xFF378A94);
+  static const Color electric = Color(0xFFE0E64B);
+  static const Color fairy = Color(0xFF9E1A44);
+  static const Color fighting = Color(0xFF9F422A);
+  static const Color fire = Color(0xFFB22328);
+  static const Color flying = Color(0xFF90B1C5);
+  static const Color ghost = Color(0xFF363069);
+  static const Color grass = Color(0xFF007C42);
+  static const Color gray21 = Color(0xFFB1A5A5);
+  static const Color ground = Color(0xFFAD7235);
+  static const Color ice = Color(0xFF7ECFF2);
+  static const Color mdAmber700 = Color(0xFFFFA000);
+  static const Color mdBlue100 = Color(0xFF81D4FA);
+  static const Color mdBlue200 = Color(0xFF0091EA);
+  static const Color mdGreen100 = Color(0xFF81C784);
+  static const Color mdGreen200 = Color(0xFF388E3C);
+  static const Color mdOrange100 = Color(0xFFFFA726);
+  static const Color mdOrange200 = Color(0xFFFF6D00);
+  static const Color mdYellow100 = Color(0xFFFFD600);
+  static const Color mdYellow200 = Color(0xFFFBC02D);
+  static const Color poison = Color(0xFF642785);
+  static const Color psychic = Color(0xFFAC296B);
+  static const Color rock = Color(0xFF4B190E);
+  static const Color skyBlue = Color(0xFF57A8D8);
+  static const Color steel = Color(0xFF5C756D);
+  static const Color water = Color(0xFF2648DC);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color white12 = Color(0xFFFAFAFA);
+  static const Color white56 = Color(0x8EFFFFFF);
+  static const Color white70 = Color(0xB2FFFFFF);
+  static const Color white87 = Color(0xDDFFFFFF);
+  static const Color white93 = Color(0xEDF8F8F8);
+  static const Color yellow = Color(0xFFFBC02D);
+}
