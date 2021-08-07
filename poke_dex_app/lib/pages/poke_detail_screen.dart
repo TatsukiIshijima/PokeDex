3ff10977faaa2fbaa383e7f3a05aa0969030b9cb
ishijimaitsuki <tatsuki445@gmail.com>
2021-08-07 17:04:54 +0900

    create poke detail screen class

---
+++ poke_dex_app/lib/pages/poke_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class PokeDetailScreen extends StatelessWidget {
+  const PokeDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('PokeDetailScreen'),
+    );
+  }
+}
