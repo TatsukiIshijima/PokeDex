5f21a1b03d228cf01edc6c0d724e40cb4c7883ea
ishijimaitsuki <tatsuki445@gmail.com>
2021-08-21 17:15:02 +0900

    add reducer

--- poke_dex_app/lib/reducers/loading_reducer.dart
+++ poke_dex_app/lib/reducers/loading_reducer.dart
@@ -1,10 +1,11 @@
+import 'package:poke_dex_app/actions/poke_detail_actions.dart';
 import 'package:poke_dex_app/actions/poke_list_actions.dart';
 import 'package:poke_dex_app/states/loading_state.dart';
 
 LoadingState loadingReducer(LoadingState oldState, dynamic action) {
   // TODO:ローディングが必要な action の追加
   // action に応じて LoadingState の状態を変更
-  if (action is FetchPokeListAction) {
+  if (action is FetchPokeListAction || action is FetchPokeDetailAction) {
     return const LoadingState(isLoading: true);
   } else {
     return const LoadingState(isLoading: false);
--- poke_dex_app/lib/reducers/poke_detail_reducer.dart
+++ poke_dex_app/lib/reducers/poke_detail_reducer.dart
@@ -1,16 +1,63 @@
 import 'package:poke_dex_app/actions/poke_detail_actions.dart';
+import 'package:poke_dex_app/actions/poke_list_actions.dart';
+import 'package:poke_dex_app/reducers/error_reducer.dart';
+import 'package:poke_dex_app/reducers/loading_reducer.dart';
 import 'package:poke_dex_app/states/poke_detail_state.dart';
 import 'package:redux/redux.dart';
 
 final pokeDetailReducer = combineReducers<PokeDetailState>([
   TypedReducer<PokeDetailState, SelectPokeAction>(_selectPokeReducer),
   TypedReducer<PokeDetailState, DeselectPokeAction>(_deselectPokeReducer),
+  TypedReducer<PokeDetailState, FetchPokeDetailAction>(_fetchPokeDetailReducer),
+  TypedReducer<PokeDetailState, ShowPokeDetailAction>(_showPokeDetailReducer),
+  TypedReducer<PokeDetailState, ShowPokeApiErrorAction>(
+      _showPokeApiErrorReducer),
 ]);
 
 PokeDetailState _selectPokeReducer(
         PokeDetailState oldState, SelectPokeAction action) =>
-    PokeDetailState(pokemon: action.pokemon);
+    PokeDetailState(selectPokemon: action.pokemon);
 
 PokeDetailState _deselectPokeReducer(
         PokeDetailState oldState, DeselectPokeAction action) =>
-    const PokeDetailState(pokemon: null);
+    const PokeDetailState(
+      selectPokemon: null,
+      pokemonInfo: null,
+    );
+
+PokeDetailState _fetchPokeDetailReducer(
+        PokeDetailState oldState, FetchPokeDetailAction action) =>
+    PokeDetailState(
+      selectPokemon: oldState.selectPokemon,
+      pokemonInfo: oldState.pokemonInfo,
+      loadingState: loadingReducer(
+        oldState.loadingState,
+        action,
+      ),
+    );
+
+PokeDetailState _showPokeDetailReducer(
+        PokeDetailState oldState, ShowPokeDetailAction action) =>
+    PokeDetailState(
+      selectPokemon: oldState.selectPokemon,
+      pokemonInfo: action.pokemonInfo,
+      loadingState: loadingReducer(
+        oldState.loadingState,
+        action,
+      ),
+    );
+
+PokeDetailState _showPokeApiErrorReducer(
+        PokeDetailState oldState, ShowPokeApiErrorAction action) =>
+    PokeDetailState(
+      selectPokemon: oldState.selectPokemon,
+      pokemonInfo: oldState.pokemonInfo,
+      loadingState: loadingReducer(
+        oldState.loadingState,
+        action,
+      ),
+      errorState: errorReducer(
+        oldState.errorState,
+        action,
+      ),
+    );
