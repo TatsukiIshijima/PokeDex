4dfc6c1022c65d34c743966f548142b1f9ca7ec5
ishijimaitsuki <tatsuki445@gmail.com>
2021-08-22 22:26:25 +0900

    create poke type view class

---
+++ poke_dex_app/lib/pages/poke_type_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:poke_dex_app/gen/colors.gen.dart';
+
+class PokeTypeView extends StatelessWidget {
+  const PokeTypeView({
+    Key? key,
+    required this.types,
+    required this.typeItemWidth,
+    required this.typeItemHeight,
+    required this.space,
+  }) : super(key: key);
+
+  final List<String> types;
+  final double typeItemWidth;
+  final double typeItemHeight;
+  final double space;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(
+        types.length,
+        (index) {
+          return Container(
+            width: typeItemWidth,
+            height: typeItemHeight,
+            margin: EdgeInsets.only(
+              right: index == types.length - 1 ? 0 : space,
+            ),
+            child: _TypeText(
+              type: types[index],
+              bgColor: ColorName.dark,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _TypeText extends StatelessWidget {
+  const _TypeText({
+    Key? key,
+    required this.type,
+    this.textColor = ColorName.white,
+    this.cornerRadius = 20.0,
+    this.borderWidth = 1.0,
+    required this.bgColor,
+  }) : super(key: key);
+
+  final String type;
+  final Color textColor;
+  final double cornerRadius;
+  final double borderWidth;
+  final Color bgColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: bgColor,
+        border: Border.all(
+          color: bgColor,
+          width: borderWidth,
+        ),
+        borderRadius: BorderRadius.circular(
+          cornerRadius,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          type,
+          style: TextStyle(
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
